01a1a82f0db5061bb0175418acdb005c55c23efa
Siwei <liusiwei.yjlo@gmail.com>
2016-10-24 19:42:46 +0800

    While loop

---
+++ 10_while_loop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+num=1
+
+while [ $num -le 10 ]; do
+    echo $num
+    num=$((num + 1))
+done
+
+num=1
+while [ $num -le 20 ]; do
+    if (( ((num % 2)) == 0 )); then
+        num=$((num + 1))
+        continue
+    fi
+    
+    if ((num >= 15)); then
+        break
+    fi
+
+    echo $num
+    num=$((num + 1))
+done
+
+num=1
+until [ $num -gt 10 ]; do
+    echo $num
+    num=$((num + 1))
+done
